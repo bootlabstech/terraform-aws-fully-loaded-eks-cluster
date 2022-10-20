9c70493a062574536ab44b1737947459a9ee2982
Naveen M <naveenm@Naveens-MacBook-Air.local>
2022-10-20 15:49:16 +0530

    changed the variables.tf files

--- eks.tf
+++ eks.tf
@@ -1,41 +1,41 @@
-locals {
-  name               = basename(path.cwd)
-  vpc_id             = module.aws_vpc.vpc_id
-  tags = {
-    Blueprint  = local.name
-    GithubRepo = "github.com/aws-ia/terraform-aws-eks-blueprints"
-  }
-  private_subnet_ids = var.private_subnet_ids
+# locals {
+#   name               = basename(path.cwd)
+#   vpc_id             = module.aws_vpc.vpc_id
+#   tags = {
+#     Blueprint  = local.name
+#     GithubRepo = "github.com/aws-ia/terraform-aws-eks-blueprints"
+#   }
+#   private_subnet_ids = var.private_subnet_ids
 
-}
+# }
 
-module "eks_blueprints" {
-  source = "./modules/eks-blueprints"
+# module "eks_blueprints" {
+#   source = "./modules/eks-blueprints"
 
-  cluster_name = local.name
+#   cluster_name = local.name
 
-  # EKS Cluster VPC and Subnets
-  vpc_id             = local.vpc_id
-  private_subnet_ids = local.private_subnet_ids
+#   # EKS Cluster VPC and Subnets
+#   vpc_id             = local.vpc_id
+#   private_subnet_ids = local.private_subnet_ids
 
-  # Cluster Security Group
-  cluster_security_group_additional_rules = var.cluster_security_group_additional_rules
+#   # Cluster Security Group
+#   cluster_security_group_additional_rules = var.cluster_security_group_additional_rules
 
-  # EKS CONTROL PLANE VARIABLES
-  cluster_version = var.cluster_version
+#   # EKS CONTROL PLANE VARIABLES
+#   cluster_version = var.cluster_version
 
-  cluster_endpoint_public_access  = false
-  cluster_endpoint_private_access = true
+#   cluster_endpoint_public_access  = false
+#   cluster_endpoint_private_access = true
 
-  # EKS MANAGED NODE GROUPS
-  managed_node_groups = {
-    mg_5 = {
-      node_group_name = "managed-ondemand"
-      instance_types  = ["m5.large"]
-      subnet_ids      = local.private_subnet_ids
-    }
-  }
+#   # EKS MANAGED NODE GROUPS
+#   managed_node_groups = {
+#     mg_5 = {
+#       node_group_name = "managed-ondemand"
+#       instance_types  = ["m5.large"]
+#       subnet_ids      = local.private_subnet_ids
+#     }
+#   }
 
-  #Custom Tags.
-  tags = local.tags
-}
+#   #Custom Tags.
+#   tags = local.tags
+# }
